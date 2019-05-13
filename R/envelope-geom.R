3a16954ce4e3bf3fe2730d2ba00f64a53e3dfca9
Jameel Alsalam <alsalam.jameel@epa.gov>
2019-05-13 10:37:18 -0400

    geom/stat for envelope, v1

--- R/envelope-geom.R
+++ R/envelope-geom.R
@@ -3,12 +3,12 @@
 #' @inheritParams ggplot2::geom_line
 #' @export
 geom_envelope <- function(mapping = NULL, data = NULL, stat = "identity",
-                      position = "stack", na.rm = FALSE, show.legend = NA,
+                      position = "identity", na.rm = FALSE, show.legend = NA,
                       inherit.aes = TRUE, ...) {
   layer(
     data = data,
     mapping = mapping,
-    stat = stat,
+    stat = StatEnvelope,
     geom = GeomEnvelope,
     position = position,
     show.legend = show.legend,
@@ -24,13 +24,45 @@ geom_envelope <- function(mapping = NULL, data = NULL, stat = "identity",
 #' @format NULL
 #' @usage NULL
 #' @export
-GeomArea <- ggproto("GeomEnvelope", GeomRibbon,
+GeomEnvelope <- ggproto("GeomEnvelope", GeomRibbon,
                     default_aes = aes(colour = NA, fill = "grey20", size = 0.5, linetype = 1,
-                                      alpha = NA),
+                                      alpha = 0.2),
                     
-                    required_aes = c("x", "y"),
-                    
-                    setup_data = function(data, params) {
-                      transform(data[order(data$PANEL, data$group, data$x), ], ymin = 0, ymax = y)
-                    }
+                    required_aes = c("x", "ymin", "ymax")
+                    # ,
+                    # 
+                    # setup_data = function(data, params) {
+                    #   transform(data[order(data$PANEL, data$group, data$x), ], ymin = 0, ymax = y)
+                    # }
 )
+
+
+#' @export
+stat_envelope <- function(mapping = NULL, data = NULL, geom = "ribbon",
+                                     position = "identity", na.rm = FALSE, show.legend = NA, 
+                                     inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatEnvelope, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+
+#' @export
+StatEnvelope <- ggproto("StatEnvelope", Stat,
+                        compute_group = function(data, scales) {
+                          data %>%
+                            group_by(x) %>%
+                            summarize(
+                              ymin = min(y),
+                              ymax = max(y)
+                            )
+                        },
+                        required_aes = c("x", "y")
+                        )
+
+
+
+
+
